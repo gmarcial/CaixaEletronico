bfd61db59ba948767b9ebbcfaa4b4182ac895842
Guilherme Felipe Ferreira Marcial <guilherme.marcial.felipe@gmail.com>
2018-01-01 02:18:20 -0300

    Criando entidade conta, testando depositar

--- src/CaixaEletronico.Core/Domain/Entities/Conta.cs
+++ src/CaixaEletronico.Core/Domain/Entities/Conta.cs
@@ -1,7 +1,36 @@
-﻿namespace CaixaEletronico.Core.Domain.Entities
+﻿using System;
+
+namespace CaixaEletronico.Core.Domain.Entities
 {
     public class Conta
     {
-        
+        public long Id { get; }
+        public long AgenciaId { get; }
+        public long PessoaId { get; }
+        public Guid Numero { get; }
+        public decimal Saldo { get; private set; }
+
+        public Conta(long id, long agenciaId, long pessoaId, Guid numero, decimal saldo = 0)
+        {
+            if (numero.Equals(null))
+                throw new ArgumentException("O numero da conta não foi gerado em sua criação", nameof(numero));
+            
+            Id = id;
+            AgenciaId = agenciaId;
+            PessoaId = pessoaId;
+            Numero = numero;
+            Saldo = saldo;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            if (valor == 0)
+                throw new ArgumentException("O valor a ser depositado não pode ser zero", nameof(valor));
+            
+            if (valor > 5000)
+                throw new ArgumentException("O valor a ser depositado excedeu o limite de 5000 reais", nameof(valor));
+
+            Saldo =+ valor;
+        }
     }
 }
---
+++ tests/CaixaEletronico.tests.UnitTests/Core/Domain/Entities/ContaTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CaixaEletronico.Core.Domain.Entities;
+using Xunit;
+
+namespace CaixaEletronico.tests.UnitTests.Core.Domain.Entities
+{
+    public class ContaTests
+    {
+        private Conta conta{ get; }
+        
+        public ContaTests()
+        {
+            conta = new Conta(83712, 9134, 1301, Guid.NewGuid());
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [InlineData(500)]
+        [InlineData(1000)]
+        [InlineData(1500)]
+        [InlineData(2000)]
+        [InlineData(2500)]
+        [InlineData(3000)]
+        [InlineData(3500)]
+        [InlineData(4000)]
+        [InlineData(4500)]
+        [InlineData(5000)]
+        public void Um_deposito_é_valido_quando_o_valor_for_abaixo_ou_igual_a_5000(decimal valor)
+        {
+            conta.Depositar(valor);
+
+            Assert.Equal(valor, conta.Saldo);
+        }
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(5001)]
+        public void Um_deposito_é_invalido_quando_o_valor_for_maior_que_5000_ou_igual_a_zero(decimal valor)
+        {
+            Assert.Throws<ArgumentException>(() => conta.Depositar(valor));
+        }
+    }
+}
