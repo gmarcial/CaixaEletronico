5f8eb2920b18abc3193d952894a940b3bdddb9f8
Guilherme Felipe Ferreira Marcial <guilherme.marcial.felipe@gmail.com>
2018-01-06 01:18:08 -0300

    Implementando TransferenciaBancaria

---
+++ src/CaixaEletronico.Core/Domain/ValueObjects/TransferenciaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CaixaEletronico.Core.Domain.Interfaces;
+
+namespace CaixaEletronico.Core.Domain.ValueObjects
+{
+    public class TransferenciaBancaria
+    {
+        public IConta Remetente { get; }
+        public IConta Favorecido { get; }
+        public decimal Valor { get; }
+        public Guid Numero { get; }
+        
+        public TransferenciaBancaria(IConta remetente, IConta favorecido, Guid numero, decimal valor = 0)
+        {
+            //TODO Encapsular validações basicas/comuns
+            if (remetente.Equals(null))
+                throw new ArgumentNullException(nameof(remetente), "O remetente esta nullo.");
+            
+            if (favorecido.Equals(null))
+                throw new ArgumentNullException(nameof(favorecido), "O favorecido esta nullo.");
+            
+            if(valor < 0)
+                throw new ArgumentOutOfRangeException(nameof(valor), "O argumento valor esta negativo");
+            
+            if (numero.Equals(null))
+                throw new ArgumentException("O numero da transferencia não foi gerado em sua criação", nameof(numero));
+            
+            if(remetente.Saldo <= 0)
+                throw new ArgumentOutOfRangeException(nameof(remetente.Saldo),
+                    "O saldo do remetente é igual a zero ou negativo");
+            
+            if(remetente.Saldo < valor)
+                throw new ArgumentOutOfRangeException(nameof(remetente.Saldo),
+                    "O saldo do remetente é inferior ao valor a ser transferido");
+
+            Remetente = remetente;
+            Favorecido = favorecido;
+            Valor = valor;
+            Numero = numero;
+        }
+
+        public void Transferir()
+        {
+            Remetente.Sacar(Valor);
+            
+            Favorecido.Depositar(Valor);
+        }
+        
+    }
+}
