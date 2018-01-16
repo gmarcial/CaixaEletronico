e19f768d95b0419e67c2c157c09506accfdcfae0
Guilherme Felipe Ferreira Marcial <guilherme.marcial.felipe@gmail.com>
2018-01-16 01:08:31 -0300

    Implementação do repositorio referente a conta

---
+++ src/CaixaEletronico.Core/Domain/Repositories/IContaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CaixaEletronico.Core.Domain.Entities;
+using CaixaEletronico.Infrastructure;
+using CaixaEletronico.Infrastructure.Logger;
+
+namespace CaixaEletronico.Core.Domain.Repositories
+{
+    public interface IContaRepository
+    {
+        ValueTask<Conta> ObterContaPeloNumeroAsync(Guid numero);
+        ValueTask<decimal> ObterSaldoAsync(Guid numero);
+        ValueTask<IEnumerable<Extrato>> ObterExtrato(Guid numero);
+    }
+}
---
+++ src/CaixaEletronico.Infrastructure.Data/Repositories/ContaRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using CaixaEletronico.Core.Domain.Entities;
+using CaixaEletronico.Core.Domain.Repositories;
+using CaixaEletronico.Infrastructure.Logger;
+using Dapper;
+
+namespace CaixaEletronico.Infrastructure.Data.Repositories
+{
+    public class ContaRepository : IContaRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ContaRepository(IDbConnection connection) => _connection = connection;
+        
+        public async ValueTask<Conta> ObterContaPeloNumeroAsync(Guid numero)
+        {
+            const string query = "select * from Conta where Numero = @numero;";
+
+            return await _connection.QuerySingleAsync<Conta>(query, numero);
+        }
+
+        public async ValueTask<decimal> ObterSaldoAsync(Guid numero)
+        {
+            const string query = "select Saldo from Conta where Numero = @numero";
+            
+            return await _connection.QuerySingleAsync<decimal>(query, numero);
+        }
+
+        public async ValueTask<IEnumerable<Extrato>> ObterExtrato(Guid numero)
+        {
+            const string query = "select * from Extrato where Numero = @numero";
+
+            return await _connection.QueryAsync<Extrato>(query, numero);
+        }
+    }
+}
