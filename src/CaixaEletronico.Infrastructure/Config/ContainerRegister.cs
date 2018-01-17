b2f1782e1805e964c23d4f29d7840e51d03200b5
Guilherme Felipe Ferreira Marcial <guilherme.marcial.felipe@gmail.com>
2018-01-17 00:05:58 -0300

    Adicionando lib simple injector, implementando um container de dependencias

--- src/CaixaEletronico.Infrastructure/Config/ContainerRegister.cs
+++ src/CaixaEletronico.Infrastructure/Config/ContainerRegister.cs
@@ -2,23 +2,21 @@
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using CaixaEletronico.Core.Domain.Repositories;
 using CaixaEletronico.Infrastructure.Data.Repositories;
 using SimpleInjector;
 
 namespace CaixaEletronico.Infrastructure.Config
 {
-    public class ContainerConfig
+    public class ContainerRegister
     {
-        public void testa()
+        public void Register(Container container)
         {
-            var container = new Container();
-
             container.Register<IDbConnection>(() =>
-                new SqlConnection(Path.Combine(Directory.GetCurrentDirectory(), "../../CaixaEletronico.Infrastructure.Data/CaixaEletronico")));
+                new SqlConnection(Path.Combine(Directory.GetCurrentDirectory(),
+                    "../../CaixaEletronico.Infrastructure.Data/CaixaEletronico")), Lifestyle.Scoped);
             container.Register<IContaRepository, ContaRepository>(Lifestyle.Singleton);
-
-            Directory.GetCurrentDirectory();
             
             container.Verify();
         }
